71c1b5854605d2f394c8cba0bb39e81dcef08eb2
Dan Stowell <dstowell@gmail.com>
2023-04-07 14:36:05 -0500

    write about page and configure site

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><b>vc build</b> explainer</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dan-stowell/vc-build-explainer',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/dan-stowell/vc-build-explainer/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: <span>powered by <a href="https://nextra.site/">Nextra</a></span>,
   },
 }
 
